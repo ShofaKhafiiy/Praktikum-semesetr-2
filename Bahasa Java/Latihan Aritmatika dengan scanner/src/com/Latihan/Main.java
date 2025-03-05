b019633f902af72595003d8336667ebb6ac79940
SSS <shofakhafidin@gmail.com>
2025-03-05 16:20:30 +0700

    Belajar scan dan membuat program hitung luas persegi

--- Bahasa Java/Latihan Aritmatika dengan scanner/src/com/Latihan/Main.java
+++ Bahasa Java/Latihan Aritmatika dengan scanner/src/com/Latihan/Main.java
@@ -1,6 +1,23 @@
 package com.Latihan;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        Scanner UserInput = new Scanner(System.in);
+
+        System.out.println("Program menghitung luas persegi");
+        System.out.print("Masukan panjang persegi: ");
+        int Panjang = UserInput.nextInt();
+        System.out.print("Masukan Lebar Persegi: ");
+        int Lebar = UserInput.nextInt();
+
+        int total = Panjang * Lebar;
+
+        System.out.println("Luas persegi panjangnya adalah: " + total);
+        UserInput.close();
+
+
+
     }
 }

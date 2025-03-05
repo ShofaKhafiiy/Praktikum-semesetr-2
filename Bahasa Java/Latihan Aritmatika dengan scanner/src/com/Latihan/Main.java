1e0e2eab1e4b98258bff9f43b1c138fe60ec2922
SSS <shofakhafidin@gmail.com>
2025-03-05 19:35:18 +0700

    memperbarui kode agar lebih ringkas

--- Bahasa Java/Latihan Aritmatika dengan scanner/src/com/Latihan/Main.java
+++ Bahasa Java/Latihan Aritmatika dengan scanner/src/com/Latihan/Main.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner UserInput = new Scanner(System.in);
+        int Panjang, Lebar, total;
 
         System.out.println("Program menghitung luas persegi");
         System.out.print("Masukan panjang persegi: ");
-        int Panjang = UserInput.nextInt();
+         Panjang = UserInput.nextInt();
         System.out.print("Masukan Lebar Persegi: ");
-        int Lebar = UserInput.nextInt();
-
-        int total = Panjang * Lebar;
+         Lebar = UserInput.nextInt();
+         total = Panjang * Lebar;
 
         System.out.println("Luas persegi panjangnya adalah: " + total);
         UserInput.close();

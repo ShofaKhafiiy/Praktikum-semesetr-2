3d5230d5f66d1b47d2d8c9fdb79b83295130dd86
SSS <shofakhafidin@gmail.com>
2025-03-17 16:41:25 +0700

    latihan class

---
+++ Latihan/Latihan Class method construct/src/latihanclass/Main.java
@@ -0,0 +1,31 @@
+package latihanclass;
+
+
+//membuat class player
+class Player{
+    String name;
+    double health;
+    int level;
+}
+
+//membuat  class senjata
+
+class Senjata{
+    double attackPower;
+}
+
+//membuat class armor
+
+class Armor {
+    double defencePower;
+}
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+
+    }
+}
